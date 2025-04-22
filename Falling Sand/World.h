cfe34ebe3ddd8d7d170fef27ffb6f51c6d5e64d6
Foster <fosterjameskiatarae@gmail.com>
2025-04-23 02:50:11 +1200

    Added more info on what world class will be

--- Falling Sand/World.h
+++ Falling Sand/World.h
@@ -1,6 +1,7 @@
 #pragma once
 // Manages the simulation grid state and the core particle update logic.
-
+// Particle type logic will be in here for easy access to both grids and all cells.
+// Split each particle update logic into own func then call all funcs in one overall func
 class World
 {
 };
